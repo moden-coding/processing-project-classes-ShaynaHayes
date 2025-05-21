4d453eb9c1f66601b84d9fad11cb68bbe5463507
ShaynaHayes <28shayes@gannacademy.org>
2025-05-20 20:41:04 -0400

    5/20/25

--- src/App.java
+++ src/App.java
@@ -10,9 +10,21 @@ public class App extends PApplet {
     public Block blockThree;
     public Block blockFour;
     ArrayList<Block> blocks;
-    ArrayList<Block>randomOrder;
-    public int random;
-   
+    ArrayList<Block> randomOrder;
+    public Block blockOneWhite;
+    public Block blockTwoWhite;
+    public Block blockThreeWhite;
+    public Block blockFourWhite;
+    public int randomNumber;
+    public int startFrame = 100;
+
+    int currentSequenceIndex = 0; // Track the user's progress in the sequence
+    boolean isUserTurn = false; // Track whether it's the user's turn to click
+    int delayBetweenBlocks = 1000;
+
+    // public int value;
+
+    // public int random;
 
     // public int mouseX;
     // public int mouseY;
@@ -30,17 +42,26 @@ public class App extends PApplet {
         // blockFour = new Block(400, 285, this, 186, 255, 244);
 
         blocks = new ArrayList<>();
-        {
-            blockOne = new Block(175, 60, this, 255, 186, 217);
-            blockTwo = new Block(400, 60, this, 255, 255, 186);
-            blockThree = new Block(175, 285, this, 213, 255, 186);
-            blockFour = new Block(400, 285, this, 186, 255, 244);
-            blocks.add(blockOne);
-            blocks.add(blockTwo);
-            blocks.add(blockThree);
-            blocks.add(blockFour);
-        }
+        randomOrder = new ArrayList<>();
 
+        blockOne = new Block(175, 60, this, 255, 186, 217);
+        blockTwo = new Block(400, 60, this, 255, 255, 186);
+        blockThree = new Block(175, 285, this, 213, 255, 186);
+        blockFour = new Block(400, 285, this, 186, 255, 244);
+        // blockOneWhite = new Block(175, 60, this, 255, 255, 255);
+        // blockTwoWhite = new Block(400, 60, this, 255, 255, 255);
+        // blockThreeWhite = new Block(175, 285, this, 255, 255, 255);
+        // blockFourWhite = new Block(400, 285, this, 255, 255, 255);
+        blocks.add(blockOne);
+        blocks.add(blockTwo);
+        blocks.add(blockThree);
+        blocks.add(blockFour);
+        // blocks.add(blockOneWhite);
+        // blocks.add(blockTwoWhite);
+        // blocks.add(blockThreeWhite);
+        // blocks.add(blockFourWhite);
+        chooseRandomBlock();
+        playSequence();
     }
 
     public void settings() {
@@ -48,7 +69,10 @@ public class App extends PApplet {
     }
 
     public void draw() {
+
+        // System.out.println(frameCount);
         if (scene == 0) {
+
             // text shown on instruction page
             textSize(80);
             background(200, 200, 200);
@@ -61,6 +85,9 @@ public class App extends PApplet {
             text(" and click them in the right order!", 13, 410);
             textSize(40);
             text("Press the space bar to start", 165, 550);
+            // if (frameCount > startFrame && frameCount < startFrame + 100) {
+            // // rect(100,100,300,300);
+            // }
         }
         if (scene == 1) {
             textSize(55);
@@ -73,8 +100,30 @@ public class App extends PApplet {
             for (Block b : blocks) {
                 b.display();
             }
-            // fill(252, 255, 186);
+
+            // if (randomNumber == 0) {
+            // fill(255);
+            // rect(175, 60, 225, 225);
+            // }
+
+            // if (randomNumber == 1) {
+            // fill(255);
             // rect(400, 60, 225, 225);
+
+            // }
+
+            // if (randomNumber == 2) {
+            // fill(255);
+            // rect(175, 285, 225, 225);
+            // }
+
+            // if (randomNumber == 3) {
+            // fill(255);
+            // rect(400, 285, 225, 225);
+
+            // }
+            // // fill(252, 255, 186);
+            // // rect(400, 60, 225, 225);
 
             // fill(186, 255, 244);
             // rect(400, 285, 225, 225);
@@ -92,17 +141,55 @@ public class App extends PApplet {
         if (key == ' ' && scene == 0) {
             scene = 1;
         }
-        if(key == 'r'){
-// random(0,4);
-System.out.println(random(0,4));
-// for (int random = 0; random < 4; random++) {
-            
-        
+        if (key == ' ' && scene == 1) {
+            // random(0,4);
+            chooseRandomBlock();
+            Block last = randomOrder.get(randomOrder.size() - 1);
+            last.blink(700);
 
-//     }
         }
+
+        // if(key == 's'){
+        // for(Block b: randomOrder){
+        // b.changeColor();
+
+        // }
+        // }
+        // if (value == 1) {
+        // blockOne += randomOrder;
+        // }
+        // if (value == 2) {
+        // blockTwo += randomOrder;
+
+        // }
+        // if(value == 3){
+
+        // }
+        // if(value == 4){
+
+        // }
     }
+
     public void mousePressed() {
+        if (!isUserTurn)
+            return; // Do nothing if it's not the user's turn
+
+        // Check if the clicked block matches the current block in the sequence
+        if (checkUserInput(mouseX, mouseY)) {
+            // If the user clicked the right block, move to the next block in the sequence
+            currentSequenceIndex++;
+            if (currentSequenceIndex == randomOrder.size()) {
+                // If the user has completed the entire sequence, add a new block and play the
+                // sequence
+                chooseRandomBlock();
+                playSequence();
+            }
+        } else {
+            // If the user clicked the wrong block, handle the error (e.g., reset the game)
+            System.out.println("Wrong block clicked! Game Over.");
+            resetGame();
+        }
+
         if (blockOne.touching(mouseX, mouseY)) {
             System.out.println("touching one");
 
@@ -120,9 +207,62 @@ System.out.println(random(0,4));
     }
 
     public void randomOrder() {
-randomOrder= new ArrayList<>(blocks);
+        randomOrder = new ArrayList<>(blocks);
 
+    }
 
+    public boolean checkUserInput(int mouseX, int mouseY) {
+        Block currentBlock = randomOrder.get(currentSequenceIndex);
+        return currentBlock.touching(mouseX, mouseY); // Check if the mouse is on the correct block
+    }
+
+    public void chooseRandomBlock() {
+        // int randomNumber = (int) random(0, 4);
+        // // Block chosen = blocks.get(randomNumber);
+        // // randomOrder.add(chosen);
+        // System.out.println(randomNumber);
+        // // for (int random = 0; random < 4; random++) {
+        // randomOrder.add(blocks.get(randomNumber));
+        int randomIndex = (int) random(0, blocks.size());
+        Block chosenBlock = blocks.get(randomIndex);
+        randomOrder.add(chosenBlock);
+        // System.out.println("Random block: " + randomNumber);
+    }
+
+    public void randomNumberOrder() {
+        // if (scene==1) {
+        // fill(255);
+        // rect(175, 60, 255, 255);
+        // }
+    }
+
+    public void playSequence() {
+        // After adding a new block, start playing the sequence
+        currentSequenceIndex = 0; // Reset user progress
+        isUserTurn = false; // Make sure it's not user turn while sequence plays
+
+        // Play back the sequence with delay between each block
+        for (int i = 0; i < randomOrder.size(); i++) {
+            Block block = randomOrder.get(i);
+            int delay = i * delayBetweenBlocks; // Delay between each block in sequence
+
+            // Blink each block for a certain period
+            int finalI = i;
+            delay(delay); // Wait before starting the next block
+            block.blink(600); // Blink block for 500ms
+
+            // Set isUserTurn to true after sequence ends
+            if (i == randomOrder.size() - 1) {
+                isUserTurn = true; // Let the user start clicking after sequence ends
+            }
+        }
+    }
+
+    public void resetGame() {
+        randomOrder.clear(); // Clear the sequence
+        chooseRandomBlock(); // Start with a new block
+        playSequence();
+        scene = 0; // Play the sequence
     }
 
 }
--- src/Block.java
+++ src/Block.java
@@ -1,4 +1,7 @@
 import processing.core.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import processing.core.PApplet;
 
 public class Block {
     private int x;
@@ -9,41 +12,84 @@ public class Block {
     private int gColor;
     private int bColor;
     private boolean touching;
+    
+
+
+    // private int startFrame = -500;
+    // private boolean normalColor = true;
+
+     private boolean isBlinking = false;
+
+    private final Timer timer = new Timer(); // Shared timer instance per block
+
+
     // private int mouseX;
     // private int mouseY;
 
     public Block(int xPos, int yPos, PApplet s, int r, int g, int b) {
-        x = xPos;
-        y = yPos;
-        size = 225;
-        screen = s;
-        rColor = r;
-        gColor = g;
-        bColor = b;
+       this. x = xPos;
+       this. y = yPos;
+       this. size = 225;
+       this. screen = s;
+      this.  rColor = r;
+       this. gColor = g;
+       this. bColor = b;
 
     }
 
     public void display() {
-        screen.fill(rColor, gColor, bColor);
+        // long time = System.currentTimeMillis();
+        if (isBlinking) {
+            // System.out.println("other color");
+            screen.fill(255);
+            // screen.fill(rColor-100,gColor - 100, bColor -100);
+            
+        } else{
+            screen.fill(rColor, gColor, bColor);
+            
+        }
         screen.rect(x, y, size, size);
+
     }
+    // public void flash(){
+    // blockOneWhite = new Block(175, 60, this, 255, 255, 255);
+
+    // }
 
     public boolean touching(int mouseX, int mouseY) {
-        if (mouseX > x && mouseX < x + size) {
-            if (mouseY > y && mouseY < y + size) {
-                return true;
-            // return mouseX > x && mouseX < x + size && mouseY > y && mouseY < y + size;
-            // else{
-            // touching=false;
-            // }
-            // }
-        }
-        
+        // if (mouseX > x && mouseX < x + size) {
+        // if (mouseY > y && mouseY < y + size) {
+        // return;
+        return mouseX > x && mouseX < x + size &&
+                mouseY > y && mouseY < y + size;
     }
-    return false;
+     public void blink(int durationMillis) {
+        isBlinking = true;
 
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                isBlinking = false;
+            }
+        }, durationMillis);
+    }
+    
+
+    // public void changeColor() {
+    //     // rColor = (int) screen.random(255);
+    //     // gColor = (int) screen.random(255);
+    //     // bColor = (int) screen.random(255);
+    //     startFrame = screen.frameCount;
+
+    // }
+    // return mouseX > x && mouseX < x + size && mouseY > y && mouseY < y + size;
+    // else{
+    // touching=false;
+    // }
+    // }
 }
-}
+
+// return touching;
 
 // if(mouseX > x && mouseX < x+size){
 // if(mouseY > y && mouseY < y+size){
